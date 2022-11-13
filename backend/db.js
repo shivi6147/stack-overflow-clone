95acd4c43c2a879faae213e63843e4a023299dfe
shivi6147 <42615654+shivi6147@users.noreply.github.com>
2022-11-13 23:44:58 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 //require('dotenv').config({ path: '..\\stack-overflow-clone\\backend\\.env' });
 const mongoose = require("mongoose");
+const MONGO_URL="mongodb+srv://shivam:luYk2SlC544ZHbB1@cluster0.g0kn3uj.mongodb.net";
 
 module.exports.connect = () => {
     mongoose
